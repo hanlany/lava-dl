9057c6c4ce9f6b0cc10dbf81a12dd70fb5171ec0
bamsumit <bam_sumit@hotmail.com>
2024-09-20 04:09:48 +0000

    Delta logic fixes
    Signed-off-by: bamsumit <bam_sumit@hotmail.com>

--- src/lava/lib/dl/conversion/activation.py
+++ src/lava/lib/dl/conversion/activation.py
@@ -43,7 +43,8 @@ class Delta(AbstractActivation):
                 delta = x[..., t] - act_last
                 y[..., t] = torch.where(torch.abs(delta) >= threshold.to(x.device),
                                         delta, 0 * delta)
-                act_last = act_last + delta
+                # act_last = act_last + delta
+                act_last = act_last + y[..., t]
         return y
 
     def forward_full(self, x):
